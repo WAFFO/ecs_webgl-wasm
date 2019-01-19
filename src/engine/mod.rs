69c32336922acec7968a30c8d005800fcb905d7b
Garrett Steffen <garrettsteffen@gmail.com>
2019-01-19 15:47:56 -0600

    Updated demo.

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -71,7 +71,7 @@ impl Engine {
             test_3d(&mut self.world, self.mesh_manager.load(String::from("debug_d20")),
                     vec3( -3.0, 0.0, -7.0 ),
                     2.0,
-                    vec3( 0.0, 0.0, -0.45 ),
+                    vec3( 1.0, 0.0, -0.45 ),
             )
         );
 
