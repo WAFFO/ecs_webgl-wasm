03c5ea12b883f48d631bbba473a55bebcf032c94
Garrett Steffen <garrettsteffen@gmail.com>
2019-03-04 00:26:19 -0600

    Added windows command for killing node.exe

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,24 @@
 set -ex
 cd "$(dirname $0)/www"
 
-trap 'kill %1' SIGINT
+unameOut="$(uname -s)";
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+function cleanup {
+    if [[ "${machine}" == "MinGw" ]] || [[ "${machine}" == "Cygwin" ]]; then
+        taskkill -F -IM node.exe
+    fi;
+    kill %1
+    kill %2
+}
+
+trap cleanup SIGINT
 cargo run --bin server 2>&1 | sed -e 's/^/[Cargo] /' \
   & npm --prefix ../client/js/ run serve 2>&1 | sed -e 's/^/[Webpack] /'
 
